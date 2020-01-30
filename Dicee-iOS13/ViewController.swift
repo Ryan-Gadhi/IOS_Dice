1328bd1ba26dfe64c15f3e33a2b75e89cd627ca7
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-01-31 02:21:33 +0300

    Added randomization feature for the dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,17 +16,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // here we are changing the picture and opacity of the two differnet images
+        // the user has not yet clicked the button
         dice_1.alpha = 0.5
-        dice_2.image = #imageLiteral(resourceName: "DiceTwo")
+        dice_2.alpha = 0.5
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         
-        dice_1.alpha = 1
-        dice_1.image = #imageLiteral(resourceName: "DiceSix")
-        dice_2.image = #imageLiteral(resourceName: "DiceSix")
+       let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),
+                #imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
         
-        print("enterd")
+        dice_1.alpha = 1
+        dice_2.alpha = 1
+        
+        // let randomInt = Int.random(in: 0..<6)
+        let random_index_1 = Int.random(in: 0..<6)
+        let random_index_2 = Int.random(in: 0..<6)
+        
+        dice_1.image = dices[random_index_1]
+        dice_2.image = dices[random_index_2]
     }
     
 }
